27925c42895a0c0edf6a7a44442c8b5bd06bb7b4
RonSamuel <109104664+RonSamuel@users.noreply.github.com>
2023-11-30 17:30:23 -0500

    Basic Admin Layout with mock data

--- src/logged_in_admin/dashboard/Dashboard.js
+++ src/logged_in_admin/dashboard/Dashboard.js
@@ -1,61 +1,109 @@
-import React, { Fragment, useEffect } from "react";
-import PropTypes from "prop-types";
-import { Typography, Box } from "@mui/material";
-import SettingsArea from "./SettingsArea";
-import UserDataArea from "./UserDataArea";
-import AccountInformationArea from "./AccountInformationArea";
-import StatisticsArea from "./StatisticsArea";
+import React, { useState } from 'react';
+import { Box, Typography, Button, List, ListItem, Divider, TextField } from '@mui/material';
 
-function Dashboard(props) {
-  const {
-    selectDashboard,
-    CardChart,
-    statistics,
-    toggleAccountActivation,
-    pushMessageToSnackbar,
-    targets,
-    setTargets,
-    isAccountActivated,
-  } = props;
+const mockCoachRequests = [
+  { id: 1, name: 'John Doe', requestDate: '2023-01-01' },
+  { id: 2, name: 'Jane Smith', requestDate: '2023-01-05' },
+];
 
-  useEffect(selectDashboard, [selectDashboard]);
+const mockExerciseBank = [
+  { id: 1, name: 'Push-ups' },
+  { id: 2, name: 'Sit-ups' },
+];
+
+const AdminDashboard = () => {
+  const [coachRequests, setCoachRequests] = useState(mockCoachRequests);
+  const [exerciseBank, setExerciseBank] = useState(mockExerciseBank);
+  const [newExercise, setNewExercise] = useState('');
+
+  const handleAcceptRequest = (requestId) => {
+    // Implement acceptance logic
+  };
+
+  const handleRejectRequest = (requestId) => {
+    // Implement rejection logic
+  };
+
+  const handleAddExercise = () => {
+    // Implement add exercise logic
+  };
 
   return (
-    <Fragment>
-      <StatisticsArea CardChart={CardChart} data={statistics} />
-      <Box mt={4}>
-        <Typography variant="subtitle1" gutterBottom>
-          Your Account
-        </Typography>
+    <Box sx={{ backgroundColor: 'black', color: 'white', p: 3 }}>
+      <Box mb={4}>
+        <Typography variant="h6">Manage Coach Requests</Typography>
+        <List>
+          {coachRequests.map((request) => (
+            <ListItem
+              key={request.id}
+              sx={{ borderBottom: '1px solid grey' }}
+            >
+              {request.name} - {request.requestDate}
+              <Button
+                sx={{
+                  ml: 1,
+                  bgcolor: 'green',
+                  '&:hover': { bgcolor: 'darkgreen' },
+                }}
+                onClick={() => handleAcceptRequest(request.id)}
+              >
+                Accept
+              </Button>
+              <Button
+                sx={{
+                  ml: 1,
+                  bgcolor: 'red',
+                  '&:hover': { bgcolor: 'darkred' },
+                }}
+                onClick={() => handleRejectRequest(request.id)}
+              >
+                Reject
+              </Button>
+            </ListItem>
+          ))}
+        </List>
       </Box>
-      <AccountInformationArea
-        isAccountActivated={isAccountActivated}
-        toggleAccountActivation={toggleAccountActivation}
-      />
-      <Box mt={4}>
-        <Typography variant="subtitle1" gutterBottom>
-          Settings
-        </Typography>
+      <Divider light />
+      <Box mt={4} display="flex" justifyContent="space-between">
+        <Box width="50%">
+          <Typography variant="h6">Add Exercise to Exercise Bank</Typography>
+          <TextField
+            label="New Exercise"
+            value={newExercise}
+            onChange={(e) => setNewExercise(e.target.value)}
+            variant="outlined"
+            sx={{
+              input: { color: 'white' },
+              label: { color: 'white' },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': { borderColor: 'white' },
+                '&:hover fieldset': { borderColor: 'white' },
+                '&.Mui-focused fieldset': { borderColor: 'white' },
+              },
+            }}
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            onClick={handleAddExercise}
+            sx={{ bgcolor: 'green', '&:hover': { bgcolor: 'darkgreen' } }}
+          >
+            Add Exercise
+          </Button>
+        </Box>
+        <Box width="50%" sx={{ overflow: 'auto', maxHeight: '300px' }}>
+          <Typography variant="h6">Exercise Bank</Typography>
+          <List>
+            {exerciseBank.map((exercise) => (
+              <ListItem key={exercise.id}>
+                {exercise.name}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Box>
-      <SettingsArea pushMessageToSnackbar={pushMessageToSnackbar} />
-      <UserDataArea
-        pushMessageToSnackbar={pushMessageToSnackbar}
-        targets={targets}
-        setTargets={setTargets}
-      />
-    </Fragment>
+    </Box>
   );
-}
-
-Dashboard.propTypes = {
-  CardChart: PropTypes.elementType,
-  statistics: PropTypes.object.isRequired,
-  toggleAccountActivation: PropTypes.func,
-  pushMessageToSnackbar: PropTypes.func,
-  targets: PropTypes.arrayOf(PropTypes.object).isRequired,
-  setTargets: PropTypes.func.isRequired,
-  isAccountActivated: PropTypes.bool.isRequired,
-  selectDashboard: PropTypes.func.isRequired,
 };
 
-export default Dashboard;
+export default AdminDashboard;
