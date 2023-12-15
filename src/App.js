b7722d8b67375ead848d5408920cf9b91a86bc4f
Ron Samuel <rs259@njit.edu>
2023-12-15 16:54:11 -0500

    Add user type check and render appropriate component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React, { Fragment, Suspense, lazy } from "react";
+import React, { Fragment, Suspense, lazy, useEffect, useState } from "react";
 import { ThemeProvider, StyledEngineProvider, CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from "./theme";
 import GlobalStyles from "./GlobalStyles";
 import Pace from "./shared/components/Pace";
-import withAuth from "./shared/components/WithAuth"
+import withAuth from "./shared/components/WithAuth";
 
 const LoggedInComponent = withAuth(lazy(() => import("./logged_in/Main")));
 
 const LoggedOutComponent = lazy(() => import("./logged_out/Main"));
 
-const LoggedInCoachComponent = lazy(() => import("./logged_in_coach/Main"));
+const LoggedInCoachComponent = withAuth(lazy(() => import("./logged_in_coach/Main")));
 
-const LoggedInAdminComponent = lazy(() => import("./logged_in_admin/Main"));
+const LoggedInAdminComponent = withAuth(lazy(() => import("./logged_in_admin/Main")));
 
 function App() {
+  const [userType, setUserType] = useState(null);
+
+  useEffect(() => {
+    const storedUserType = localStorage.getItem('user_type');
+    setUserType(storedUserType);
+  }, []);
+
+  let ComponentToRender;
+
+  switch(userType) {
+    case 'coach':
+      ComponentToRender = LoggedInCoachComponent;
+      break;
+    case 'admin':
+      ComponentToRender = LoggedInAdminComponent;
+      break;
+    default:
+      ComponentToRender = LoggedInComponent;
+  }
+
   return (
     <BrowserRouter>
       <StyledEngineProvider injectFirst>
@@ -25,7 +45,7 @@ function App() {
           <Suspense fallback={<Fragment />}>
             <Switch>
               <Route path="/c">
-                <LoggedInComponent />
+                <ComponentToRender />
               </Route>
               <Route>
                 <LoggedOutComponent />
