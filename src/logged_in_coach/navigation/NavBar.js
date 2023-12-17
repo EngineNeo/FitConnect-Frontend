d10486bc3219d2149a2bbea3d8ca901704b83bfd
Ron Samuel <rs259@njit.edu>
2023-12-17 14:01:19 -0500

    fixed coach navbar to include more tabs

--- src/logged_in_coach/navigation/NavBar.js
+++ src/logged_in_coach/navigation/NavBar.js
@@ -1,12 +1,10 @@
-import React, { Fragment, useRef, useCallback, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, Fragment, useRef, useCallback, useState } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 import {
   AppBar,
   Toolbar,
   Typography,
-  Avatar,
   Drawer,
   List,
   IconButton,
@@ -16,21 +14,33 @@ import {
   Hidden,
   Tooltip,
   Box,
+  Menu,
+  MenuItem
 } from "@mui/material";
 import withStyles from "@mui/styles/withStyles";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
-import MenuIcon from "@mui/icons-material/Menu";
 import MessagePopperButton from "./MessagePopperButton";
 import SideDrawer from "./SideDrawer";
 import NavigationDrawer from "../../shared/components/NavigationDrawer";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import {Email, RecentActors} from "@mui/icons-material";
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import Cookies from 'js-cookie';
+import UserImage from "../../shared/components/UserImage";
+
+// Icons
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import MenuIcon from "@mui/icons-material/Menu";
+import SportsIcon from '@mui/icons-material/Sports';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Email from '@mui/icons-material/Email';
+import RecentActors from '@mui/icons-material/RecentActors';
+
+const logo = "/images/logged_out/FitConnectLogo.png"
 
 const styles = (theme) => ({
   appBar: {
-    boxShadow: theme.shadows[6],
-    backgroundColor: theme.palette.common.white,
+    boxShadow: 'none',
+    backgroundColor: theme.palette.common.darkBlack,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
@@ -43,6 +53,7 @@ const styles = (theme) => ({
   appBarToolbar: {
     display: "flex",
     justifyContent: "space-between",
+    backgroundColor: theme.palette.common.darkBlack,
     paddingLeft: theme.spacing(1),
     paddingRight: theme.spacing(1),
     [theme.breakpoints.up("sm")]: {
@@ -58,10 +69,13 @@ const styles = (theme) => ({
       paddingRight: theme.spacing(4),
     },
   },
+  logoContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
   accountAvatar: {
-    backgroundColor: theme.palette.secondary.main,
-    height: 24,
-    width: 24,
+    height: 42,
+    width: 42,
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     [theme.breakpoints.down("sm")]: {
@@ -105,8 +119,10 @@ const styles = (theme) => ({
     backgroundColor: `${theme.palette.primary.main} !important`,
   },
   brandText: {
-    fontFamily: "'Baloo Bhaijaan', cursive",
-    fontWeight: 400,
+    fontFamily: "'Roboto', cursive",
+    fontStyle: "bold",
+    fontWeight: 650,
+    marginLeft: "10px"
   },
   username: {
     paddingLeft: 0,
@@ -123,20 +139,85 @@ const styles = (theme) => ({
 });
 
 function NavBar(props) {
-  const { messages, classes, theme } = props;
+  const { selectedTab, setSelectedTab, messages, classes, theme } = props;
   // Will be use to make website more accessible by screen readers
   const links = useRef([]);
   const [isMobileOpen, setIsMobileOpen] = useState(false);
   const [isSideDrawerOpen, setIsSideDrawerOpen] = useState(false);
   const isWidthUpSm = useMediaQuery(theme.breakpoints.up("sm"));
-  const [selectedTab, setSelectedTab] = useState("Dashboard");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    // Function to handle tab changes
-  const handleTabChange = useCallback((newTab) => {
-    setSelectedTab(newTab);
-    // Additional logic for navigating to the new tab's content
-    // can be added here if necessary
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const location = useLocation();
+  const currentPathname = location.pathname;
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const tabMap = {
+      "/c/dashboard": "Dashboard",
+      "/c/coaches": "Coaches",
+      "/c/workoutplan": "Workout Plan",
+      "/c/my-requests": "My Requests",
+      "/c/my-clients": "My Clients",
+    };
+
+    setSelectedTab(tabMap[currentPathname]);
+  }, [currentPathname]);
+
+  const navigate = useCallback((path) => {
+    history.push(path);
+    handleMenuClose();
+  }, [history]);
+
+  const handleLogout = useCallback(async () => {
+  const userId = localStorage.getItem('user_id');
+
+    if (userId) {
+      try {
+        const response = await fetch('http://localhost:8000/fitConnect/logout/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ user_id: userId }),
+        });
+
+        if (response.ok) {
+          // Clearing local storage and cookies
+          localStorage.clear();
+          Cookies.remove('authToken');
+          history.push('/login');
+        } else {
+          console.error('Logout failed');
+          console.log(response)
+        }
+      } catch (error) {
+        console.error('Error during logout:', error);
+      }
+    }
+  }, [history]);
+
+  useEffect(() => {
+    const storedFirstName = localStorage.getItem('first_name');
+    const storedLastName = localStorage.getItem('last_name');
+    if (storedFirstName) {
+      setFirstName(storedFirstName);
+    }
+    if (storedLastName) {
+      setLastName(storedLastName);
+    }
   }, []);
+
 
   const openMobileDrawer = useCallback(() => {
     setIsMobileOpen(true);
@@ -151,68 +232,120 @@ function NavBar(props) {
   }, [setIsSideDrawerOpen]);
 
   const menuItems = [
-  {
-    link: "/",
-    name: "Dashboard",
-    onClick: () => handleTabChange("Dashboard"),
-    icon: {
-      desktop: (
-        <DashboardIcon
-          className={
-            selectedTab === "Dashboard" ? classes.textPrimary : "text-white"
-          }
-          fontSize="small"
-        />
-      ),
-      mobile: <DashboardIcon className="text-white" />,
+    {
+      link: "/c/dashboard",
+      name: "Dashboard",
+      onClick: closeMobileDrawer,
+      icon: {
+        desktop: (
+          <DashboardIcon
+            className={
+              selectedTab === "Dashboard" ? classes.textPrimary : "text-white"
+            }
+            fontSize="small"
+          />
+        ),
+        mobile: <DashboardIcon className="text-white" />,
+      },
     },
-  },
-  {
-    link: "/my-requests",
-    name: "My Requests",
-    onClick: () => handleTabChange("My Requests"),
-    icon: {
-      desktop: (
-        <Email
-          className={
-            selectedTab === "My Requests" ? classes.textPrimary : "text-white"
-          }
-          fontSize="small"
-        />
-      ),
-      mobile: <Email className="text-white" />,
+    {
+      link: "/c/coaches",
+      name: "Coaches",
+      onClick: closeMobileDrawer,
+      icon: {
+        desktop: (
+          <SportsIcon
+            className={
+              selectedTab === "Coaches" ? classes.textPrimary : "text-white"
+            }
+            fontSize="small"
+          />
+        ),
+        mobile: <SportsIcon className="text-white" />,
+      },
     },
-  },
-  {
-    link: "/my-clients",
-    name: "My Clients",
-    onClick: () => handleTabChange("My Clients"),
-    icon: {
-      desktop: (
-        <RecentActors
-          className={
-            selectedTab === "My Clients" ? classes.textPrimary : "text-white"
-          }
-          fontSize="small"
-        />
-      ),
-      mobile: <RecentActors className="text-white" />,
+    {
+      link: "/c/workoutplan",
+      name: "Workout Plan",
+      onClick: closeMobileDrawer,
+      icon: {
+        desktop: (
+          <FitnessCenterIcon
+            className={
+              selectedTab === "Workout Plan" ? classes.textPrimary : "text-white"
+            }
+            fontSize="small"
+          />
+        ),
+        mobile: <FitnessCenterIcon className="text-white" />,
+      },
     },
-  },
-  {
-    link: "/",
-    name: "Logout",
-    icon: {
-      desktop: (
-        <PowerSettingsNewIcon className="text-white" fontSize="small" />
-      ),
-      mobile: <PowerSettingsNewIcon className="text-white" />,
+    {
+      link: "/c/my-requests",
+      name: "My Requests",
+      onClick: closeMobileDrawer,
+      icon: {
+        desktop: (
+          <Email
+            className={
+              selectedTab === "My Requests" ? classes.textPrimary : "text-white"
+            }
+            fontSize="small"
+          />
+        ),
+        mobile: <Email className="text-white" />,
+      },
     },
-  },
-];
+    {
+      link: "/c/my-clients",
+      name: "My Clients",
+      onClick: closeMobileDrawer,
+      icon: {
+        desktop: (
+          <RecentActors
+            className={
+              selectedTab === "My Clients" ? classes.textPrimary : "text-white"
+            }
+            fontSize="small"
+          />
+        ),
+        mobile: <RecentActors className="text-white" />,
+      },
+    },
+  ];
+
+  const accountItems = [
+    {
+      link: "/c/dashboard",
+      name: "Dashboard",
+      onClick: () => navigate("/c/dashboard"),
+      icon: {
+        desktop: <DashboardIcon className="text-white" fontSize="small" />,
+        mobile: <DashboardIcon className="text-white" />,
+      },
+    },
+    {
+      link: "/c/account",
+      name: "Account",
+      onClick: () => navigate("/c/account"),
+      icon: {
+        desktop: <AccountCircleIcon className="text-white" fontSize="small" />,
+        mobile: <AccountCircleIcon className="text-white" />,
+      },
+    },
+    {
+      name: "Logout",
+      onClick: handleLogout,
+      icon: {
+        desktop: <ExitToAppIcon className="text-white" fontSize="small" />,
+        mobile: <ExitToAppIcon className="text-white" />,
+      },
+    },
+  ];
+
   return (
     <Fragment>
-      <AppBar position="sticky" className={classes.appBar}>
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar className={classes.appBarToolbar}>
           <Box display="flex" alignItems="center">
             <Hidden smUp>
@@ -228,22 +361,26 @@ function NavBar(props) {
               </Box>
             </Hidden>
             <Hidden smDown>
-              <Typography
-                variant="h4"
-                className={classes.brandText}
-                display="inline"
-                color="primary"
-              >
-                Fit
-              </Typography>
-              <Typography
-                variant="h4"
-                className={classes.brandText}
-                display="inline"
-                color="secondary"
-              >
-                Connect
-              </Typography>
+              <div className={classes.logoContainer}>
+                <Link
+                  to="/"
+                >
+                  <img alt="FitConnect" src={logo} style={{ width: "40px" }} />
+                </Link>
+                <Link
+                  to="/"
+                  style={{ textDecoration: 'none' }}
+                >
+                  <Typography
+                    variant="h4"
+                    className={classes.brandText}
+                    display="inline"
+                    color="secondary"
+                  >
+                  FitConnect
+                  </Typography>
+                </Link>
+              </div>
             </Hidden>
           </Box>
           <Box
@@ -253,29 +390,58 @@ function NavBar(props) {
             width="100%"
           >
             <MessagePopperButton messages={messages} />
-            <ListItem
-              disableGutters
-              className={classNames(classes.iconListItem, classes.smBordered)}
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+              color="inherit"
             >
-              <Avatar
-                alt="profile picture"
-                src={`${process.env.PUBLIC_URL}/images/logged_in/profilePicture.jpg`}
-                className={classNames(classes.accountAvatar)}
+              <UserImage
+                className={classes.accountAvatar}
+                iconSize={23}
+                name={`${firstName} ${lastName}`}
               />
               {isWidthUpSm && (
                 <ListItemText
                   className={classes.username}
                   primary={
-                    <Typography color="textPrimary">Username</Typography>
+                    <Typography color="textPrimary">{firstName || 'User'}</Typography>
                   }
                 />
               )}
-            </ListItem>
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+              style={{ marginTop: '45px' }}
+            >
+              {accountItems.map((item) => (
+                <MenuItem key={item.name} onClick={item.onClick}>
+                  <ListItemIcon>
+                    {item.icon.desktop}
+                  </ListItemIcon>
+                  <Typography variant="inherit">{item.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
           </Box>
           <SideDrawer open={isSideDrawerOpen} onClose={closeDrawer} />
         </Toolbar>
       </AppBar>
       <Hidden smDown>
+        {location.pathname !== "/" && location.pathname !== "/login" && (
         <Drawer //  both drawers can be combined into one for performance
           variant="permanent"
           classes={{
@@ -283,45 +449,42 @@ function NavBar(props) {
           }}
           open={false}
         >
-          <List>
-            {menuItems.map((element, index) => (
-              <Link
-                to={element.link}
-                className={classes.menuLink}
-                onClick={element.onClick}
-                key={index}
-                ref={(node) => {
-                  links.current[index] = node;
-                }}
-              >
-                <Tooltip
-                  title={element.name}
-                  placement="right"
-                  key={element.name}
+            <List>
+              {menuItems.map((element, index) => (
+                <Link
+                  to={element.link}
+                  className={classes.menuLink}
+                  onClick={() => {
+                    console.log(element.name);
+                    element.onClick();               
+                    props.setSelectedTab(element.name);
+                  }}
+                  key={index}
+                  ref={(node) => {
+                    links.current[index] = node;
+                  }}
                 >
-                  <ListItem
-                    selected={selectedTab === element.name}
-                    button
-                    divider={index !== menuItems.length - 1}
-                    className={classes.permanentDrawerListItem}
-                    onClick={() => {
-                      links.current[index].click();
-                    }}
-                    aria-label={
-                      element.name === "Logout"
-                        ? "Logout"
-                        : `Go to ${element.name}`
-                    }
+                  <Tooltip
+                    title={element.name}
+                    placement="right"
+                    key={element.name}
                   >
-                    <ListItemIcon className={classes.justifyCenter}>
-                      {element.icon.desktop}
-                    </ListItemIcon>
-                  </ListItem>
-                </Tooltip>
-              </Link>
-            ))}
-          </List>
+                    <ListItem
+                      selected={selectedTab === element.name}
+                      button
+                      divider={index !== menuItems.length - 1}
+                      className={classes.permanentDrawerListItem}
+                    >
+                      <ListItemIcon className={classes.justifyCenter}>
+                        {element.icon.desktop}
+                      </ListItemIcon>
+                    </ListItem>
+                  </Tooltip>
+                </Link>
+              ))}
+            </List>
         </Drawer>
+          )}
       </Hidden>
       <NavigationDrawer
         menuItems={menuItems.map((element) => ({
@@ -340,10 +503,8 @@ function NavBar(props) {
 }
 
 NavBar.propTypes = {
-  messages: PropTypes.arrayOf(PropTypes.object).isRequired,
-  selectedTab: PropTypes.string.isRequired,
+  // messages: PropTypes.arrayOf(PropTypes.object).isRequired,
   classes: PropTypes.object.isRequired,
-  openAddBalanceDialog: PropTypes.func.isRequired,
 };
 
 export default withStyles(styles, { withTheme: true })(NavBar);
