b81559211e1a1b518477e48d1cddbf244eaff22e
Ron Samuel <rs259@njit.edu>
2023-12-19 12:29:55 -0500

    fixed some connections for myrequests, end point changes for accpet/deny need to be made

--- src/logged_in_coach/dashboard/MyRequests.js
+++ src/logged_in_coach/dashboard/MyRequests.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Typography,
@@ -8,24 +8,63 @@ import {
   CardContent,
   CardActions,
   Container,
+  Grid,
 } from "@mui/material";
-
-// mock for testing without the backend
-const mockClientRequests = [
-  { id: 1, name: "John Doe" },
-  { id: 2, name: "Jane Smith" },
-  { id: 3, name: "Alice Johnson" },
-];
 
 function MyRequests() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [clientRequests, setClientRequests] = useState([]);
+  const coachId = localStorage.getItem('user_id');
+
+  useEffect(() => {
+    // Fetch client requests from the backend
+    fetch(`http://localhost:8000/fitConnect/coaches/${coachId}/requests`)
+      .then(response => response.json())
+      .then(data => {
+        console.log("Fetched client requests data:", data); // Log the data to the console
+        setClientRequests(Array.isArray(data) ? data : [data]);
+      })
+      .catch(error => console.error('Error fetching client requests:', error));
+  }, [coachId]);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredRequests = mockClientRequests.filter((client) =>
-    client.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const handleAccept = (userId) => {
+    const payload = { user: userId, coach:  coachId  };
+    fetch("http://localhost:8000/fitConnect/acceptClient/", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Client accepted:", data);
+      //   refresh requests list
+    })
+    .catch(error => console.error('Error accepting client:', error));
+  };
+
+  const handleDecline = (userId) => {
+    // do decline logic here
+    const payload = { user: userId, coach: coachId };
+    fetch("http://localhost:8000/fitConnect/declineClient/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Client declined:", data);
+      // refresh list
+    })
+    .catch(error => console.error('Error declining client:', error));
+  };
+
+  const filteredRequests = clientRequests.filter((client) =>
+    `${client.first_name} ${client.last_name}`.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    client.email.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
@@ -44,19 +83,27 @@ function MyRequests() {
           InputProps={{ style: { color: 'white' } }}
         />
       </Box>
-      {filteredRequests.map((request) => (
-        <Card key={request.id} variant="outlined" sx={{ mb: 2, bgcolor: 'grey.900', color: 'white' }}>
-          <CardContent>
-            <Typography variant="subtitle1">{request.name}</Typography>
-          </CardContent>
-          <CardActions>
-            <Button variant="contained" color="success">
-              Accept
-            </Button>
-            <Button variant="contained" color="error">
-              Deny
-            </Button>
-          </CardActions>
+      {filteredRequests.map((request, index) => (
+        <Card key={index} variant="outlined" sx={{ mb: 2, bgcolor: 'grey.900', color: 'white' }}>
+          <Grid container>
+            <Grid item xs={8}>
+              <CardContent>
+                <Typography variant="subtitle1">{`${request.first_name} ${request.last_name}`}</Typography>
+                <Typography variant="body2" sx={{ color: 'grey.500' }}>Email: {request.email}</Typography>
+                <Typography variant="body2" sx={{ color: 'grey.500' }}>Goal: {request.goal}</Typography>
+              </CardContent>
+            </Grid>
+            <Grid item xs={4}>
+              <CardActions sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                <Button variant="contained" color="success" sx={{ mx: 1 }}  onClick={() => handleAccept(request.id)}>
+                  Accept
+                </Button>
+                <Button variant="contained" color="error" sx={{ mx: 1 }}  onClick={() => handleDecline(request.id)}>
+                  Deny
+                </Button>
+              </CardActions>
+            </Grid>
+          </Grid>
         </Card>
       ))}
     </Container>
