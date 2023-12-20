e1fcfff303b4ab6c9649da89d28b1ff57c78c6dd
Ron Samuel <rs259@njit.edu>
2023-12-19 22:53:50 -0500

    fixed auto refresh

--- src/logged_in_coach/navigation/MessageHistory.js
+++ src/logged_in_coach/navigation/MessageHistory.js
@@ -2,41 +2,29 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { List, ListItem, TextField, Button } from "@mui/material";
 
-const MessageHistory = ({ history, onSendMessage, onBack, senderId, recipientId }) => {
+const MessageHistory = ({ history, onBack, senderId, recipientId }) => {
     const [newMessage, setNewMessage] = useState('');
     const [messageHistory, setMessageHistory] = useState([]);
 
-    const updateMessageHistory = (newHistory) => {
-        setMessageHistory(newHistory);
-      };
-
-      const fetchAndUpdateMessageHistory = () => {
+    const fetchAndUpdateMessageHistory = () => {
         if (recipientId !== null) {
             fetch(`http://localhost:8000/fitConnect/get_messages/${senderId}/${recipientId}/`)
                 .then(response => response.json())
-                .then(data => {
-                    setMessageHistory(data.messages);
-                })
+                .then(data => setMessageHistory(data.messages))
                 .catch(error => console.error('Error fetching messages:', error));
         }
     };
-    
+
     useEffect(() => {
         fetchAndUpdateMessageHistory();
-    }, [senderId, recipientId]); 
+    }, [senderId, recipientId]);
 
     const handleSendMessage = () => {
-        console.log("Sender ID:", senderId, "Recipient ID:", recipientId, "Message:", newMessage);
-        if(newMessage.trim() === "") {
+        if (newMessage.trim() === "") {
             console.error('Empty message cannot be sent');
             return;
         }
-        const newMessageObject = {
-            sender_id: senderId,
-            recipient_id: recipientId,
-            message_text: newMessage
-        };
-        
+
         fetch('http://localhost:8000/fitConnect/create_message/', {
             method: 'POST',
             headers: {
@@ -51,23 +39,22 @@ const MessageHistory = ({ history, onSendMessage, onBack, senderId, recipientId 
         .then(response => response.json())
         .then(data => {
             if(data.status === 'success') {
-                setNewMessage(''); 
-                onSendMessage();
+                setNewMessage('');
+                fetchAndUpdateMessageHistory(); // Fetch and update message history after sending a new message
             } else {
                 console.error('Error sending message:', data);
             }
         })
         .catch(error => console.error('Error sending message:', error));
     };
-    
 
     return (
         <div>
-            <Button onClick={onBack}>Back to Users</Button> {/* Use onBack here */}
+            <Button onClick={onBack}>Back to Users</Button>
             <List>
-                {history.map((message, index) => (
+                {messageHistory.map((message, index) => (
                     <ListItem key={index}>
-                        <b>{message.sender}:</b> {message.text}
+                        <b>{message.sender_name}:</b> {message.text}
                     </ListItem>
                 ))}
             </List>
