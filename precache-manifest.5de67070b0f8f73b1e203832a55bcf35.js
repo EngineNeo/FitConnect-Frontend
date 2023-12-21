15ab92051256fab1b0b49774fc2588775283e80e
unknown <neomaralit@gmail.com>
2023-12-21 10:56:21 -0500

    Updates

---
+++ precache-manifest.5de67070b0f8f73b1e203832a55bcf35.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aaeb84a42aaa2543a42",
+    "url": "/FitConnect-Frontend/static/js/0.87057239.chunk.js"
+  },
+  {
+    "revision": "1ba34cb77982d793fa34",
+    "url": "/FitConnect-Frontend/static/js/1.27b8dd87.chunk.js"
+  },
+  {
+    "revision": "09a0f529bc2f8208a84e",
+    "url": "/FitConnect-Frontend/static/js/2.c1324fd5.chunk.js"
+  },
+  {
+    "revision": "b2ccd5496ba9b8ce7db6",
+    "url": "/FitConnect-Frontend/static/js/3.9c1f1336.chunk.js"
+  },
+  {
+    "revision": "ff50665d44bccd354270",
+    "url": "/FitConnect-Frontend/static/js/main.e21da22a.chunk.js"
+  },
+  {
+    "revision": "335413f897ca44629975",
+    "url": "/FitConnect-Frontend/static/js/runtime~main.be3671ab.js"
+  },
+  {
+    "revision": "de4246abdb1c8fedc1d1",
+    "url": "/FitConnect-Frontend/static/js/6.fb2f1bf6.chunk.js"
+  },
+  {
+    "revision": "3ef77d7ea3b1d13a8b1d",
+    "url": "/FitConnect-Frontend/static/css/7.fc820bd1.chunk.css"
+  },
+  {
+    "revision": "3ef77d7ea3b1d13a8b1d",
+    "url": "/FitConnect-Frontend/static/js/7.011c7394.chunk.js"
+  },
+  {
+    "revision": "653b07b5f6aaf7dc12fd",
+    "url": "/FitConnect-Frontend/static/js/8.e14ef025.chunk.js"
+  },
+  {
+    "revision": "fc1d442c6bb0a1e72518",
+    "url": "/FitConnect-Frontend/static/js/9.5b7b6f54.chunk.js"
+  },
+  {
+    "revision": "d52e7a3b6901d43baea9",
+    "url": "/FitConnect-Frontend/static/js/10.f2fd80f5.chunk.js"
+  },
+  {
+    "revision": "52686fb84ff2346a964c",
+    "url": "/FitConnect-Frontend/static/js/11.77996be8.chunk.js"
+  },
+  {
+    "revision": "d9d1407f2aa89ef372e4",
+    "url": "/FitConnect-Frontend/static/js/12.fa1603fd.chunk.js"
+  },
+  {
+    "revision": "c8fda08c0c1ffad4a5e26a98065837c7",
+    "url": "/FitConnect-Frontend/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/FitConnect-Frontend/precache-manifest.5de67070b0f8f73b1e203832a55bcf35.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/FitConnect-Frontend/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
